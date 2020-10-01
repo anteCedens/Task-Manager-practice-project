1e0c7d616c848e9c146d8975dc641ccb494e82e5
anteCedens <jaksa.juric@yahoo.com.hr>
2020-10-01 12:02:17 +0000

    Added app.config MONGO_DBNAME and _URI to app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
 from flask import Flask
+from flask_pymongo import Pymongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'task_manager'
+app.config['MONGO_URI'] = 'mongodb+srv://root:<password>@cluster0.6aiov.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
 
 @app.route('/')
