38cfc76627d272b12f61636a5f88d1199b1d313e
anteCedens <jaksa.juric@yahoo.com.hr>
2020-10-02 11:13:03 +0000

    Setting environment variables: creating env.py and adding it to .gitignore; adding code to app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import os
+from os import path
 from flask import Flask
 from flask_pymongo import Pymongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'task_manager'
-app.config['MONGO_URI'] = 'mongodb+srv://root:<password>@cluster0.6aiov.mongodb.net/<dbname>?retryWrites=true&w=majority'
+if path.exists('env.py'):
+    import env
+
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 
 
 @app.route('/')
