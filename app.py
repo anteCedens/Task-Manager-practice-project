ed78ba91fa0a08ca7c6424ffeab5641403265448
anteCedens <jaksa.juric@yahoo.com.hr>
2020-10-14 01:42:55 +0000

    Missing path import in app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, request
+from os import path
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
