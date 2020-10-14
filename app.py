3ec3fbb995f023c35dcdcb6fa9e479a82d1d0533
anteCedens <jaksa.juric@yahoo.com.hr>
2020-10-14 01:41:27 +0000

    Missing app = Flask in app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, redirect, request, url_for, request
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
+app = Flask(__name__)
+
 if path.exists('env.py'):
     import env
 
